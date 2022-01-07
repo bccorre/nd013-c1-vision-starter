d3b41ae9f68aa85bd51dc771a2ef142373494d1d
bccorre <bccorre@gmail.com>
2022-01-07 11:16:24 +0000

    imporve create splits

--- create_splits.py
+++ create_splits.py
@@ -17,6 +17,7 @@ def split(data_dir):
     args:
         - data_dir [str]: data directory, /home/workspace/data/waymo
     """
+    # first clean up existing files
     data_dir_list = data_dir.split("/")
     output_dir = "/".join(data_dir_list[:-3]) if data_dir_list[-1] == "" else "/".join(data_dir_list[:-2])
     train_path = os.path.join(output_dir,"train")
@@ -28,22 +29,35 @@ def split(data_dir):
         shutil.rmtree(val_path)
     if os.path.exists(test_path):
         shutil.rmtree(test_path)
+        
+    # create new directories
     os.mkdir(train_path)
     os.mkdir(val_path)
     os.mkdir(test_path)
+    
+    #get list of records to use
     record_files = glob(data_dir+"*.tfrecord")
-    # read list of selected files
+    
+    # read list of selected files, previously selected in the exploratory data analysis
     sel_files = open("selected_files.txt","r")
+    
     # get the list of files not in the selected list
     record_list = []
     for r_file in record_files:
         if r_file not in sel_files:
             record_list.append(r_file)
     n_records = len(record_list)
+    
+    # shuffle the record list
     random.shuffle(record_list)
-    n_train = 0
+    n_train = 0 # count how many records are used for training
     n_val = 0
     n_test = 0
+    
+    # split the files into train, test, and validation
+    # for training it is used 75% of the records
+    # for validation 15%
+    # and for test 10%
     for idx, element in enumerate(record_list):
         filename = element.split("/")[-1]
         if idx // np.ceil(0.75 * n_records) == 0:
@@ -58,7 +72,8 @@ def split(data_dir):
             # add to test folder
             os.symlink(element, test_path + "/" + filename)
             n_test += 1
-    # split the seledted files into all sets
+            
+    # split the seledted files into all sets keeping the same proportion
     sel_files_sz = len(list(sel_files))
     for idx, element in enumerate(sel_files):
         filename = element.split("/")[-1]
@@ -74,7 +89,7 @@ def split(data_dir):
             # add to test folder
             os.symlink(element, test_path + "/" + filename)
             n_test += 1
-        
+    
     print(n_train,n_val,n_test)
         
 
